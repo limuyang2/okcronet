68a91f123d5b8505125840938b63fc40980dcc0b
limuyang <limuyang2@hotmail.com>
2024-03-27 14:52:39 +0800

    feat: gradle

--- okcronet/build.gradle.kts
+++ okcronet/build.gradle.kts
@@ -1,6 +1,13 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+
+    `maven-publish`
+    signing
 }
 
 android {
@@ -38,4 +45,98 @@ dependencies {
     implementation(libs.squareup.okio)
 
     compileOnly(libs.cronet.api)
+}
+
+//---------- maven upload info -----------------------------------
+
+val versionName = "1.0.0"
+
+var signingKeyId = ""//签名的密钥后8位
+var signingPassword = ""//签名设置的密码
+var secretKeyRingFile = ""//生成的secring.gpg文件目录
+
+
+val localProperties: File = project.rootProject.file("local.properties")
+
+if (localProperties.exists()) {
+    println("Found secret props file, loading props")
+    val properties = Properties()
+
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+    signingKeyId = properties.getProperty("signing.keyId")
+    signingPassword = properties.getProperty("signing.password")
+    secretKeyRingFile = properties.getProperty("signing.secretKeyRingFile")
+
+} else {
+    println("No props file, loading env vars")
+}
+
+afterEvaluate {
+
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components.findByName("release"))
+                groupId = "io.github.limuyang2"
+                artifactId = "okcronet"
+                version = versionName
+
+                pom {
+                    name.value("okcronet")
+                    description.value("A network request library similar to OKHTTP, implemented using Cronet")
+                    url.value("https://github.com/limuyang2/okcronet")
+
+                    licenses {
+                        license {
+                            //协议类型
+                            name.value("The MIT License")
+                            url.value("https://github.com/limuyang2/okcronet/blob/main/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.value("limuyang2")
+                            name.value("limuyang")
+                            email.value("limuyang2@hotmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.value("scm:git@github.com:limuyang2/okcronet.git")
+                        developerConnection.value("scm:git@github.com:limuyang2/okcronet.git")
+                        url.value("https://github.com/limuyang2/okcronet")
+                    }
+                }
+            }
+
+        }
+
+        repositories {
+            maven {
+                setUrl("$rootDir/RepoDir")
+            }
+        }
+
+
+
+    }
+
+}
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+
+        allprojects {
+            extra["signing.keyId"] = signingKeyId
+            extra["signing.secretKeyRingFile"] = secretKeyRingFile
+            extra["signing.password"] = signingPassword
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
